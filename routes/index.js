bdae88e37fddde529e2842a88e236bea393de40c
Karolin Flach <karolin.flach@gmail.com>
2019-11-17 00:21:22 +0100

    refactors and adds routes

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,10 @@
 
 const express = require('express')
 const router = express.Router();
-const RestaurantService = require('../services/restaurant-service')
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-  res.render('index', { title: 'Foodsnob Homepage' });
+  res.render('index', { title: 'Foodsnob' });
 });
-
-// restaurant functionality
-router.get('/restaurants', async (req, res) => {
-    const restaurants = await RestaurantService.findAll()
-    res.render('restaurants', { restaurants: restaurants }) 
-})
-
-router.get('/restaurants/:id', async (req, res) => {
-    const id = req.params.id
-    const restaurant = await RestaurantService.find(id)
-    res.render('restaurants', { restaurant: restaurant })
-})
-
-router.post('/submit-restaurant', async (req, res) => {
-    await RestaurantService.add(req.body)
-    res.send('ok')
-  })
-  
-router.delete('/restaurants/:id', async (req, res) => {
-    const restaurant = await RestaurantService.del(req.params.id)
-    res.send('ok')
-  })
 
 module.exports = router;
---
+++ routes/restaurants.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const RestaurantService = require('../services/restaurant-service')
+const RatingService = require('../services/rating-service')
+
+
+// restaurant endpoints
+router.get('/', async (req, res) => {
+    const restaurants = await RestaurantService.findAll()
+    res.render('restaurants', { restaurants: restaurants }) 
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    const restaurant = await RestaurantService.find(id)
+    const avgRating = await RestaurantService.getAvgRating(restaurant)
+    res.render('restaurants', { restaurant: restaurant, avgRating: avgRating })
+})
+
+router.post('/submit', async (req, res) => {
+    await RestaurantService.add(req.body)
+    res.send('ok')
+  })
+
+router.post('/:id/tag', async (req, res) => {
+    const restaurant = await RestaurantService.find(req.params.id)
+    await RestaurantService.tagRestaurant(restaurant, req.body.tags)
+    res.send(req.body)
+  })
+  
+router.delete('/:id', async (req, res) => {
+    await RestaurantService.del(req.params.id)
+    res.send('ok')
+  })
+
+
+module.exports = router;
