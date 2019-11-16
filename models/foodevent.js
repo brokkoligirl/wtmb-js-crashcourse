24303f64ccf6834dea1dfd67b404a5e06612fdd5
Karolin Flach <karolin.flach@gmail.com>
2019-11-17 00:19:25 +0100

    updates models for mongodb compatibility

--- models/foodevent.js
+++ models/foodevent.js
@@ -1,9 +1,30 @@
-module.exports = class FoodEvent {
-    constructor(eventName, eventHost, eventDate, maxAttendees, eventAttendees = []) {
-        this.eventName = eventName;
-        this.eventHost = eventHost;
-        this.eventDate = eventDate;
-        this.maxAttendees = maxAttendees;
-        this.eventAttendees = eventAttendees;
+const mongoose = require('mongoose')
+
+const EventSchema = new mongoose.Schema({
+    host : {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'Restaurant',
+    },
+    patrons : [{
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'FoodSnob',
+        max: maxAttendees,
+        autopopulate: {
+            maxDepth: 1
+        }
+    }],
+    date: {
+        type: Date,
+        min: Date.now        
+    },
+    maxAttendees: {
+        type: Number,
+        min: 1,
     }
-}
+})
+
+EventSchema.plugin(require('mongoose-autopopulate'))
+
+const EventModel = new mongoose.model('Event', EventSchema)
+
+module.exports = EventModel
--- models/rating.js
+++ models/rating.js
@@ -1,8 +1,33 @@
-module.exports = class Rating {
-    constructor(restaurant, patron, rating) {
-        this.restaurant = restaurant
-        this.patron = patron
-        this.rating = rating
-        this.visits = 1
+
+const mongoose = require('mongoose')
+
+const RatingSchema = new mongoose.Schema({
+    restaurant: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'Restaurant',
+        autopopulate: {
+            maxDepth: 2
+        }
+    },
+    patron: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'FoodSnob',
+        autopopulate: {
+            maxDepth: 2
+        }
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10,
+    },
+    visits: {
+        type: Number,
     }
-}
+})          
+
+RatingSchema.plugin(require('mongoose-autopopulate'))
+
+const RatingModel = new mongoose.model('Rating', RatingSchema)
+
+module.exports = RatingModel
