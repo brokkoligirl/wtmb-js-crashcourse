904fd20d8ee844bd2ee2394592e1dd42ddaf84ef
Karolin Flach <karolin.flach@gmail.com>
2019-11-17 00:22:18 +0100

    updates/edits views and adds forms

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const bodyParser = require('body-parser')
+const restaurantRouter = require('./routes/restaurants');
+
+require('./mongo-connection')
 
 const app = express();
 
@@ -23,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/restaurants', restaurantRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
