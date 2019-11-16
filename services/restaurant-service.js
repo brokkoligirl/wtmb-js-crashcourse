406a2d4ec361334bdb730aa3eb43d750f1fbb0d7
Karolin Flach <karolin.flach@gmail.com>
2019-11-17 00:20:46 +0100

    updates services

--- services/restaurant-service.js
+++ services/restaurant-service.js
@@ -2,8 +2,26 @@ const BaseService = require('./base-service')
 const RestaurantModel = require('../models/restaurant')
 
 class RestaurantService extends BaseService {
-    constructor() {
-        super(RestaurantModel, `${__dirname}/../exports/restaurants.json`)
+    model = RestaurantModel
+
+    getAvgRating(restaurant) {
+        var ratingSum = 0;
+        restaurant.ratings.forEach(mySum);
+        function mySum(item) {
+            ratingSum += item.rating;
+        }
+        return ratingSum / restaurant.ratings.length;
+    }
+
+    async tagRestaurant(restaurant, newTags) {
+        let nnewTags = newTags.toString().split(',')
+        let currentTags = restaurant.tags
+        for (let tag of nnewTags) {
+            if (!currentTags.includes(tag)) {
+                restaurant.tags.push(tag);
+            }
+        }
+        await restaurant.save()
     }
 }
 
