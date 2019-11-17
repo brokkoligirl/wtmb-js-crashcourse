2a6304508eba3bce9501a83c788a81fe281ad026
Karolin Flach <karolin.flach@gmail.com>
2019-11-17 21:06:06 +0100

    adds routes for reservations

--- routes/users.js
+++ routes/users.js
@@ -1,43 +1,59 @@
 const express = require('express');
 const router = express.Router();
+
 const FoodSnobService = require('../services/foodsnob-service')
 const RestaurantService = require('../services/restaurant-service')
 const RatingService = require('../services/rating-service')
+const ReservationService = require('../services/reservation-service')
 
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
-    const people = await FoodSnobService.findAll()
-    res.render('foodsnobs', { people: people })
-  })
-  
+  const people = await FoodSnobService.findAll()
+  res.render('foodsnobs', { people: people })
+})
+
 router.get('/:id', async (req, res) => {
-    const id = req.params.id
-    const person = await FoodSnobService.find(id)
-    res.render('foodsnobs', { person: person })
-  })
+  const id = req.params.id
+  const person = await FoodSnobService.find(id)
+  res.render('foodsnobs', { person: person })
+})
 
-router.post('/submit', async (req, res) => {
-    const user = await FoodSnobService.add(req.body)
-    res.send(user)
-  })
+router.post('/new', async (req, res) => {
+  const user = await FoodSnobService.add(req.body)
+  res.send(user)
+})
 
-router.post('/:id/submit-visit', async (req, res) => {
-    console.log(req.body)
-    const restaurantID = req.body.restaurantID
-    const rating = parseInt(req.body.rating)
-    const foodSnobID = req.params.id
+// submit new rating
+router.post('/:id/ratings', async (req, res) => {
+  const rating = parseInt(req.body.rating)
+  const restaurant = await RestaurantService.find(req.body.restaurantID)
+  const foodSnob = await FoodSnobService.find(req.params.id)
+  
+  RatingService.rateRestaurant(foodSnob, restaurant, rating)
+  res.send(foodSnob)
+})
 
-    const restaurant = await RestaurantService.find(restaurantID)
-    const foodSnob = await FoodSnobService.find(foodSnobID)
+// save new rezzie
+router.post('/:id/reservations', async (req, res) => {
+  const patron = await FoodSnobService.find(req.params.id)
+  const restaurant = await RestaurantService.find(req.body.restaurantID)
+  const date = ReservationService.convertDate(req.body.date, req.body.time)
+  
+  const newRezzie = {
+    "restaurant": restaurant,
+    "patron": patron,
+    "date": date,
+    "partySize": parseInt(req.body.partySize)
+  }
 
-    RatingService.rateRestaurant(foodSnob, restaurant, rating)
-    
-    res.send(foodSnob)
-  })
+  await ReservationService.saveReservation(patron, newRezzie)
+
+  res.send("rezzie saved")
+})
 
 router.delete('/:id', async (req, res) => {
-    await FoodSnobService.del(req.params.id)
-    res.send('ok')
-  })
+  await FoodSnobService.del(req.params.id)
+  res.send('ok')
+})
 
 module.exports = router;
