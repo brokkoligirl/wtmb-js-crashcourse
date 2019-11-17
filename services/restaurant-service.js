c68bfec20ad7f641ce46c61324da1acf039317ed
Karolin Flach <karolin.flach@gmail.com>
2019-11-17 21:03:03 +0100

    adds method for fetching restaurant locations

--- services/restaurant-service.js
+++ services/restaurant-service.js
@@ -1,5 +1,7 @@
 const BaseService = require('./base-service')
 const RestaurantModel = require('../models/restaurant')
+const NodeGeocoder = require('node-geocoder')
+
 
 class RestaurantService extends BaseService {
     model = RestaurantModel
@@ -22,6 +24,34 @@ class RestaurantService extends BaseService {
             }
         }
         await restaurant.save()
+    }
+
+    async getRestaurantCoords(restaurant) {
+        const geocoder = NodeGeocoder({ provider: 'openstreetmap', });
+        try {
+            let loc = await geocoder.geocode(`${restaurant.name}, Berlin`)
+            console.log(loc)
+            if (loc.length == 0) {
+                console.log('no location found')
+                return
+            }
+            // TODO: un-hardcode Berlin in search query
+            restaurant.location = {
+                latitude: loc[0].latitude,
+                longitude: loc[0].longitude,
+                formattedAddress: loc[0].formattedAddress,
+                country: loc[0].country,
+                city: loc[0].city,
+                state: loc[0].state,
+                zipcode: loc[0].zipcode,
+                streetName: loc[0].streetName,
+                streetNumber: loc[0].streetNumber,
+                countryCode: loc[0].countryCode,
+                neighbourhood: loc[0].neighbourhood,
+            }
+        } catch {
+            console.log(Error);
+        }
     }
 }
 
