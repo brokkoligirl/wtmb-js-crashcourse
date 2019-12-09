9a4d6aeae69d32226789dde030f62f8d661b5164
Karolin Flach <karolin.flach@gmail.com>
2019-12-09 16:09:03 +0100

    moved files to backend

--- services/event-service.js
+++
@@ -1,8 +0,0 @@
-const BaseService = require('./base-service')
-const EventModel = require('../models/foodevent')
-
-class EventService extends BaseService {
-    model = EventModel
-}        
-
-module.exports = new EventService()
